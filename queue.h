d24934ea06f6a506763026cbf5985ce3c15209ba
mauriciocroquet17 <83285675+mauriciocroquet17@users.noreply.github.com>
2021-09-12 21:20:13 -0500

    Add files via upload

--- queue.h
+++ queue.h
@@ -1,6 +1,88 @@
 #include "circulararray.h"
 
+// -------------------------------------------------------------------------------------------- Stack
 template <typename T>
-class QueueArray : public CircularArray<T> { 
-    
+class QueueArray : public CircularArray<T> {
+private:
+    T* array{};
+    int capacity{}, size{};
+    T front{}, back{};
+public:
+    void push_front(T data){
+        if(size == 0){
+            capacity =+ 3;
+            array[0] = data;
+            size++;
+        }
+        else{
+            if(capacity == size)
+                capacity+= 3;
+            T temp = data;
+            T hold;
+            for(int i = 0; i < size; i++){
+                hold = array[i];
+                array[i] = temp;
+                temp = hold;
+            }
+        }
+        front = data;
+    }
+    void push_back(T data){
+        if(size == 0){
+            capacity =+ 3;
+            array[0] = data;
+            size++;
+        }
+        else{
+            if(capacity == size)
+                capacity+= 3;
+            array[size + 1] = data;
+            size++;
+        }
+        back = data;
+    }
+    T pop_front(){
+        if(size == 0)
+            return 0;
+        else{
+            return front;
+        }
+    }
+    T pop_back(){
+        T temp = array[size];
+        array[size] = 0;
+        size--;
+        return temp;
+    }
+    bool is_full(){
+        if(size == capacity)
+            return true;
+        else{
+            return false;
+        }
+    }
+    bool is_empty(){
+        if(size == 0)
+            return true;
+        else{
+            return false;
+        }
+    }
+    void clear(){
+        for(int i = 0; i < capacity; i++){
+            array[i] = 0;
+        }
+        front = 0;
+        back = 0;
+        size = 0;
+    }
+    void reverse(){
+        T *newArray = new T[capacity];
+        for(int i = 0; i < size; i++){
+            newArray[i] = array[size - i];
+        }
+        front = array[size];
+        back = array[0];
+        array = newArray;
+    }
 };
--- stack.h
+++ stack.h
@@ -1,6 +1,63 @@
 #include "circulararray.h"
 
+// -------------------------------------------------------------------------------------------- Stack
 template <typename T>
-class StackArray : public CircularArray<T> { 
-    
+class StackArray : public CircularArray<T> {
+private:
+    T* array{};
+    int capacity{}, size{};
+    T front{};
+public:
+    void push_front(T data){
+        if(size == 0){
+            capacity =+ 3;
+            array[0] = data;
+            size++;
+        }
+        else{
+            if(capacity == size)
+                capacity+= 3;
+            T temp = data;
+            T hold;
+            for(int i = 0; i < size; i++){
+                hold = array[i];
+                array[i] = temp;
+                temp = hold;
+            }
+        }
+        front = data;
+    }
+    T pop_front(){
+        if(size == 0)
+            return 0;
+        else{
+            return front;
+        }
+    }
+    bool is_full() {
+        if (capacity == size)
+            return true;
+        return false;
+    }
+
+    bool is_empty(){
+        if(size == 0)
+            return true;
+        return false;
+    }
+    void clear(){
+        for(int i = 0; i < capacity; i++){
+            array[i] = 0;
+        }
+        front = 0;
+        size = 0;
+    }
+    void reverse(){
+        T *newArray = new T[capacity];
+        for(int i = 0; i < size; i++){
+            newArray[i] = array[size - i];
+        }
+        front = array[size];
+        array = newArray;
+    }
 };
